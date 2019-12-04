d725ba07330fbcc4fe590ef5cb347bd53dd9ff98
shpark <guriguri1576@gmail.com>
2019-12-05 01:34:23 +0900

    vscode에서 lombok이 갑자기 안되서 삽질...
    기억하자.
    "java.jdt.ls.vmargs": "-javaagent:/data/dev/java/lombok.jar -Xbootclasspath/a:/data/dev/java/lombok.jar"

---
+++ src/main/java/com/bingbingpa/events/EventStatus.java
@@ -0,0 +1,6 @@
+package com.bingbingpa.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/com/bingbingpa/events/EventTest.java
@@ -0,0 +1,11 @@
+package com.bingbingpa.events;
+
+import org.junit.jupiter.api.Test;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder().build();
+    }
+}
