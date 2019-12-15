68ef4692633d66dc6c8a7b239a713932c074f9f4
shpark <guriguri1576@gmail.com>
2019-12-15 18:52:48 +0900

    resource 관련 주석 추가

--- src/main/java/com/bingbingpa/events/EventResource.java
+++ src/main/java/com/bingbingpa/events/EventResource.java
@@ -7,6 +7,9 @@ import org.springframework.hateoas.Link;
 
 public class EventResource extends EntityModel<Event> {
 	
+	/*
+	 * self link는 resource마다 설정해줘야 하므로 여기에 공통으로 추가한다.
+	 */
 	public EventResource(Event event, Link... links) {
 		super(event,  links);
 		add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
