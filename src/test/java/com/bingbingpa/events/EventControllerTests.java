7764c66036d1a671b84fb69f94ead8becd5079da
shpark <guriguri1576@gmail.com>
2019-12-17 01:51:22 +0900

    restdoc 테스트 코드 추가

--- src/test/java/com/bingbingpa/events/EventControllerTests.java
+++ src/test/java/com/bingbingpa/events/EventControllerTests.java
@@ -1,6 +1,11 @@
 package com.bingbingpa.events;
 
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -73,8 +78,57 @@ public class EventControllerTests {
             .andExpect(jsonPath("_links.self").exists())
             .andExpect(jsonPath("_links.query-events").exists())
             .andExpect(jsonPath("_links.update-event").exists())
-            .andDo(document("create-event"))
-            ;
+            .andDo(document("create-event",
+            		links(
+            				linkWithRel("self").description("link to self"),
+            				linkWithRel("query-events").description("link to query events"),
+            				linkWithRel("update-event").description("link to update an existing event")
+            		),
+            		requestHeaders(
+            				headerWithName(HttpHeaders.ACCEPT).description("accept header"),
+            				headerWithName(HttpHeaders.CONTENT_TYPE).description("content type header")
+    				),
+            		requestFields(
+            				fieldWithPath("name").description("Name of new event"),
+            				fieldWithPath("description").description("description of new event"),
+            				fieldWithPath("beginEnrollmentDateTime").description("begin enroll time"),
+            				fieldWithPath("closeEnrollmentDateTime").description("close enroll time"),
+            				fieldWithPath("beginEventDateTime").description("begin event time"),
+            				fieldWithPath("endEventDateTime").description("end event time"),
+            				fieldWithPath("location").description("location of new event"),
+            				fieldWithPath("basePrice").description("base price of new event"),
+            				fieldWithPath("maxPrice").description("max price of new event"),
+            				fieldWithPath("limitOfEnrollment").description("limit of enrollment")
+    				),
+            		responseHeaders(
+            				headerWithName(HttpHeaders.LOCATION).description("Location header"),
+                            headerWithName(HttpHeaders.CONTENT_TYPE).description("Content type")
+    				),
+            		/**
+            		 * relaxedResponseFields 를 쓰면 모든 필드를 기술할 필요가 없다. 
+            		 * 하지만 모든 필드를 기술하지 않으므로 정확한 문서를 만들지 못한다. 
+            		 * responseFields를 쓰면 모든 필드를 기술해야 한다. 
+            		 */
+            		responseFields(
+            				fieldWithPath("id").description("Identifier of new event"),
+            				fieldWithPath("name").description("Name of new event"),
+            				fieldWithPath("description").description("description of new event"),
+            				fieldWithPath("beginEnrollmentDateTime").description("begin enroll time"),
+            				fieldWithPath("closeEnrollmentDateTime").description("close enroll time"),
+            				fieldWithPath("beginEventDateTime").description("begin event time"),
+            				fieldWithPath("endEventDateTime").description("end event time"),
+            				fieldWithPath("location").description("location of new event"),
+            				fieldWithPath("basePrice").description("base price of new event"),
+            				fieldWithPath("maxPrice").description("max price of new event"),
+            				fieldWithPath("limitOfEnrollment").description("limit of enrollment"),
+            				fieldWithPath("free").description("it tells if this event is free or not"),
+            				fieldWithPath("offline").description("it tells if this event is offline or not"),
+            				fieldWithPath("eventStatus").description("event status"),
+            				fieldWithPath("_links.self.href").description("link to self"),
+                            fieldWithPath("_links.query-events.href").description("link to query event list"),
+                            fieldWithPath("_links.update-event.href").description("link to update existing event")
+    				)
+            ));
     }
     
     @Test
