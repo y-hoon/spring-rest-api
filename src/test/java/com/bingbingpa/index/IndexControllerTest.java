e6da95320377c105ff6e31dc8fbbc4a98a2516fe
shpark <guriguri1576@gmail.com>
2019-12-28 10:56:32 +0900

    index document 추가 asciidoc operation이 안되서 include로 변경

--- src/test/java/com/bingbingpa/index/IndexControllerTest.java
+++ src/test/java/com/bingbingpa/index/IndexControllerTest.java
@@ -1,5 +1,8 @@
 package com.bingbingpa.index;
 
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +27,9 @@ public class IndexControllerTest extends BaseControllerTest {
 	public void index() throws Exception {
 		this.mockMvc.perform(get("/api"))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("_links.events").exists());
+			.andExpect(jsonPath("_links.events").exists())
+			.andDo(document("index",
+            		links(
+            				linkWithRel("events").description("event list"))));
 	}
 }
